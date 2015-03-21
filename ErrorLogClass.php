1f81c0bf2b32d413cb4f7efff00c3cb3cbb41962
liuwm91 <372203429@qq.com>
2015-03-21 12:48:19 +0800

    写log改为静态方法

--- ErrorLogClass.php
+++ ErrorLogClass.php
@@ -1,21 +1,31 @@
 <?php
 	class ErrorLog{
-		private $path;
+		private static $path;
 
-		public function __construct(){
+		// public function __construct(){
+		// 	if(!file_exists('log/log.txt')){
+  //               mkdir('log');
+		// 	}
+		// 	$this->path = "log/log.txt";  
+
+		// }
+
+		private function setPath(){
 			if(!file_exists('log/log.txt')){
                 mkdir('log');
 			}
-			$this->path = "log/log.txt";  
-
+			self::$path = "log/log.txt";  			
 		}
 
-		public function writeLog($log){
+		public static function writeLog($log){
+			if(empty(self::$path)){
+				self::setPath();				
+			}
 			$content = date('Y-m-d H:i:s',time());
 			$content .= ":\n";
 			$content .= var_export($log,true);
 			$content .= "\n";
-			file_put_contents($this->path, $content,FILE_APPEND);
-			file_put_contents($this->path, "========================================\n",FILE_APPEND);
+			file_put_contents(self::$path, $content,FILE_APPEND);
+			file_put_contents(self::$path, "========================================\n",FILE_APPEND);
 		}
 	}
