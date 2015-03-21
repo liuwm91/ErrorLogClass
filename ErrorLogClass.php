16ccf211b19d44101628abc2b02259808fad195f
liuwm91 <372203429@qq.com>
2015-03-21 12:28:59 +0800

    修改显示方式

--- ErrorLogClass.php
+++ ErrorLogClass.php
@@ -3,21 +3,19 @@
 		private $path;
 
 		public function __construct(){
-			if(file_exists('log/log.txt')){
-				$this->path = "log/log.txt";
-			}else{
-                            mkdir('log');
+			if(!file_exists('log/log.txt')){
+                mkdir('log');
 			}
-			
 			$this->path = "log/log.txt";  
 
 		}
 
-		public function writeLog($content){
-			if(is_array($content)){
-				$content = print_r($content,true);
-			}
-			$content .="\n";
+		public function writeLog($log){
+			$content = date('Y-m-d H:i:s',time());
+			$content .= ":\n";
+			$content .= var_export($log,true);
+			$content .= "\n";
 			file_put_contents($this->path, $content,FILE_APPEND);
+			file_put_contents($this->path, "========================================\n",FILE_APPEND);
 		}
 	}
